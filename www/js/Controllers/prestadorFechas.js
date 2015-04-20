111cf7636dde045dc6feb2941d9799aa48b6bd0b
chibchombiano@outlook.com <chibchombiano@outlook.com>
2015-04-20 02:05:45 -0500

    20 de abril

--- www/js/Controllers/prestadorFechas.js
+++ www/js/Controllers/prestadorFechas.js
@@ -1,7 +1,7 @@
 angular.module('starter')
 .controller('prestadorFechasCtrl', 
-    ['$scope', 'dataTableStorageFactory', '$ionicLoading', 'users', '$state','varsFactoryService', '$stateParams', '$cordovaDatePicker',
-	function ($scope, dataTableStorageFactory, $ionicLoading, users, $state, varsFactoryService, $stateParams, $cordovaDatePicker) {
+    ['$scope', 'dataTableStorageFactory', '$ionicLoading', 'users', '$state','varsFactoryService', '$stateParams', '$cordovaDatePicker', 'pushFactory', 'emailFactory',
+	function ($scope, dataTableStorageFactory, $ionicLoading, users, $state, varsFactoryService, $stateParams, $cordovaDatePicker, pushFactory, emailFactory) {
 	$scope.shouldShowDelete = false;
     $scope.shouldShowReorder = false;
     $scope.listCanSwipe = true
@@ -34,7 +34,7 @@ angular.module('starter')
         var currentPlatform = ionic.Platform.platform();
         var currentPlatformVersion = ionic.Platform.version();
 
-        if(!isWindowsPhone){
+        if(isAndroid || isIOS || isIPad){
              $cordovaDatePicker.show(options).then(function(date){
                 $scope.datosCita.fecha = date;
                 console.log(date);
@@ -47,16 +47,21 @@ angular.module('starter')
     $scope.solicitarCita = function(){
         var usuario = users.getCurrentUser();
         var item = varsFactoryService.prestadorSeleccionado();
+        var platformPush = pushFactory.getPlatform();
 
         var data = {
             prestador : item.username,
+            platform : platformPush,
             PartitionKey : item.username,
             RowKey: usuario.username,
             solicitadaPor : usuario.username,
             nombreTabla: 'TmCitas',
-            fecha : $scope.datosCita.fecha            
+            fecha : $scope.datosCita.fecha
         }
 
+        var textoCita = 'Nueva cita solicitada';
+        pushFactory.enviarMensajePlatform(item.email,textoCita, item.platform);
+        emailFactory.enviarEmail(usuario.email, item.email, 'Cita solicitada', textoCita, textoCita);
         dataTableStorageFactory.saveStorage(data);
     }
 }])
--- www/lib/hefesoft.azure/Azure/push.js
+++ www/lib/hefesoft.azure/Azure/push.js
@@ -1,14 +1,13 @@
 angular.module('starter')
 .factory('pushFactory', ['$http','urlServicioFactory' , '$q', '$rootScope','$cordovaPush', 
   function ($http, urlServicioFactory, $q, $rootScope, $cordovaPush) {
-	
-	var urlBase = urlServicioFactory.getUrlBase();
+  
+  var urlBase = urlServicioFactory.getUrlBase();
     var dataFactory = {};
 
     var androidConfig = {
         "senderID": "505952414500",
     };
- 
 
 
     $rootScope.$on('$cordovaPush:notificationReceived', function(event, notification) {
@@ -56,9 +55,9 @@ angular.module('starter')
     }
 
     dataFactory.register = function(key){
-    	var deferred = $q.defer();
-    	var key = key;
-    	$http.post(urlBase + "api/register", {key : key}).success(function (data) {                
+      var deferred = $q.defer();
+      var key = key;
+      $http.post(urlBase + "api/register", {key : key}).success(function (data) {                
                 updateRegister(data, deferred, key);
             })
             .error(function (error) {
@@ -70,34 +69,49 @@ angular.module('starter')
 
     //Un tag o un nombre
     dataFactory.enviarMensaje = function(enviarA, mensaje){
-
-    	var item = { platform : "gcm", to_tag : enviarA, mensaje : mensaje};
-    	$http.post(urlBase + "api/notifications", item)
-    	.success(function (data) {                
-        	console.log(data);        
+      var platformVersion = getPlatform();
+      var item = { platform : platformVersion, to_tag : enviarA, mensaje : mensaje};
+      $http.post(urlBase + "api/notifications", item)
+      .success(function (data) {                
+          console.log(data);        
         })
         .error(function (error) {
-          	console.log(error);      
+            console.log(error);      
         });
     }
 
-	return  dataFactory;
+    dataFactory.enviarMensajePlatform = function(enviarA, mensaje, platform){      
+      var item = { platform : platform, to_tag : enviarA, mensaje : mensaje};
+      $http.post(urlBase + "api/notifications", item)
+      .success(function (data) {                
+          console.log(data);        
+        })
+        .error(function (error) {
+            console.log(error);      
+        });
+    }
 
-	function updateRegister(data, promise, key){
-		var deferred = promise;
-		var tag = "Pacientes, Odontologia";
-		var platform = getPlatform();
+  dataFactory.getPlatform = function(){
+      return getPlatform();    
+  }
 
-		//Falta habilitar para m
-		var item ={idhubazure : data, tag : tag, platform: platform, key : key};
+  return  dataFactory;
 
-		var req = {
+  function updateRegister(data, promise, key){
+    var deferred = promise;
+    var tag = "Pacientes, Odontologia";
+    var platform = getPlatform();
+
+    //Falta habilitar para m
+    var item ={idhubazure : data, tag : tag, platform: platform, key : key};
+
+    var req = {
                 method: 'PUT', 
                 url: urlBase + "api/register",
                 data: item,
                 headers: {'Content-Type': 'application/x-www-form-urlencoded',
                 'Content-Type': 'application/json'
-            	}
+              }
             }
             $http(req).success(function(data) { 
                 console.log(data);               
@@ -108,20 +122,26 @@ angular.module('starter')
                 console.log(data);
                 deferred.reject(data);                 
             });
-	}
+  }
 
+  function getPlatform(){
+      var deviceInformation = ionic.Platform.device();
+      var isAndroid = ionic.Platform.isAndroid();
+      var isWebView = ionic.Platform.isWebView();
+      var isIPad = ionic.Platform.isIPad();
+      var isIOS = ionic.Platform.isIOS();
+      var isAndroid = ionic.Platform.isAndroid();
+      var isWindowsPhone = ionic.Platform.isWindowsPhone();
 
-
-	function getPlatform(){
-    	var deviceInformation = ionic.Platform.device();
-        var isAndroid = ionic.Platform.isAndroid();
+      var currentPlatform = ionic.Platform.platform();
+      var currentPlatformVersion = ionic.Platform.version();
 
       if(isAndroid){
-  			return  "gcm";
-	  }
+        return  "gcm";
+    }
       else{
-      	return "gcm";
+        return "gcm";
       }
-	}
+  }
 
 }])
